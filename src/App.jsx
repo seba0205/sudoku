f0e52c6a21410e7c96361a9d007d2eceff904bc4
seba0205 <83637951+seba0205@users.noreply.github.com>
2024-03-07 11:21:58 +1100

    implemented basic method for difficulty level

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,7 @@ function App() {
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
   ];
-  return (
-    <div>
-      <Grid array={test} />
-    </div>
-  );
+  return <Grid array={test} />;
 }
 
 export default App;
--- src/Grid.jsx
+++ src/Grid.jsx
@@ -4,21 +4,18 @@ import Cell from "./Cell.jsx";
 const Grid = ({ array }) => {
   //build a sudoku grid from an array of numbers
   //cell blocks are : [x < 3 && y < 3] [3 < x < 6 && y < 3] [6 < x < 9 && y < 3] ...... etc TODO: figure out a way to represent this neatly
-  //TODO: change this to use array.map
+  //TODO: work out a method of choosing whether or not a cell is mutable
+  const difficulty = 50;
 
   console.log("testgrid");
   const result = array.map((row, i) =>
     row.map((cell, j) => {
-      return <Cell trueValue={cell} mutable={true} row={i} col={j} box={1} />;
+      const mutable = Math.random() * 100 > difficulty;
+      return (
+        <Cell trueValue={cell} mutable={mutable} row={i} col={j} box={1} />
+      );
     })
   );
-  //   for (let row = 0; row > 9; row++) {
-  //     for (let col = 0; col > 9; col++) {
-  //       const box = Math.floor(row / 3) + Math.floor(col / 3) + 1;
-  //       console.log(box);
-  //       <Cell trueValue={array[row][col]} row={row} col={col} box={box} />;
-  //     }
-  //   }
 
   return <div className="grid">{result}</div>;
 };
